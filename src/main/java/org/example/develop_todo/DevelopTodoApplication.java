de1f6fcadb44793cf314396785194e15d0c2914f
star-pooh <eunki92@gmail.com>
2024-12-13 16:09:56 +0900

    Init : Create Develop Todo 일정관리 과제(develop.ver) 프로젝트 생성

---
+++ src/main/java/org/example/develop_todo/DevelopTodoApplication.java
@@ -0,0 +1,13 @@
+package org.example.develop_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopTodoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DevelopTodoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/develop_todo/DevelopTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.develop_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopTodoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
