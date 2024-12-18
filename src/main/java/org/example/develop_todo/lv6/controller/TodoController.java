27a6925a6c3d24a70793dfa9877419a8ea2bc9ff
star-pooh <eunki92@gmail.com>
2024-12-18 12:50:18 +0900

    Feat : Create Lv6 Feature Lv6 기능 추가 - 비밀번호 암호화 로직 추가 - 관련 기능 로직 수정   - 사용자 생성   - 사용자 정보 수정   - 로그인

---
+++ src/main/java/org/example/develop_todo/lv6/controller/TodoController.java
@@ -0,0 +1,131 @@
+package org.example.develop_todo.lv6.controller;
+
+import jakarta.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.example.develop_todo.lv6.dto.todo.TodoCreateRequestDto;
+import org.example.develop_todo.lv6.dto.todo.TodoResponseDto;
+import org.example.develop_todo.lv6.dto.todo.TodoUpdateRequestDto;
+import org.example.develop_todo.lv6.service.TodoService;
+import org.example.develop_todo.lv6.util.Parse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/todos")
+@RequiredArgsConstructor
+public class TodoController {
+
+  private final TodoService todoService;
+
+  /**
+   * 일정 생성 API
+   *
+   * @param dto 등록에 사용할 요청 데이터
+   * @return 등록된 일정 정보
+   */
+  @PostMapping
+  public ResponseEntity<Map<String, Object>> createTodo(
+      @Valid @RequestBody TodoCreateRequestDto dto, BindingResult bindingResult) {
+    Map<String, Object> response = new HashMap<>();
+
+    // validation 에러가 있는 경우, 메시지 출력
+    if (bindingResult.hasErrors()) {
+      String errorMessage = Parse.toString(bindingResult.getFieldErrors());
+      response.put("validationError", errorMessage);
+
+      return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    TodoResponseDto todoResponseDto =
+        this.todoService.createTodo(dto.getUserId(), dto.getTitle(), dto.getContents());
+    response.put("todoData", todoResponseDto);
+
+    return new ResponseEntity<>(response, HttpStatus.CREATED);
+  }
+
+  /**
+   * 일정 전체 조회 API
+   *
+   * @return 조회된 일정 정보
+   *     <p>일정 정보가 1건도 없을 시, 빈 배열 반환
+   */
+  @GetMapping
+  public ResponseEntity<Map<String, List<TodoResponseDto>>> findAllTodo() {
+    Map<String, List<TodoResponseDto>> response = new HashMap<>();
+
+    List<TodoResponseDto> todoResponseDtoList = this.todoService.findAllTodo();
+    response.put("todoData", todoResponseDtoList);
+
+    return new ResponseEntity<>(response, HttpStatus.OK);
+  }
+
+  /**
+   * 일정 1건 조회 API
+   *
+   * @param id 일정 ID
+   * @return 조회된 일정 정보
+   */
+  @GetMapping("/{id}")
+  public ResponseEntity<Map<String, TodoResponseDto>> findById(@PathVariable Long id) {
+    Map<String, TodoResponseDto> response = new HashMap<>();
+
+    TodoResponseDto todoResponseDto = this.todoService.findById(id);
+    response.put("todoData", todoResponseDto);
+
+    return new ResponseEntity<>(response, HttpStatus.OK);
+  }
+
+  /**
+   * 일정 수정 API
+   *
+   * @param id 일정 ID
+   * @param dto 수정에 사용할 요청 데이터
+   * @return 수정된 일정 정보
+   */
+  @PatchMapping("/{id}")
+  public ResponseEntity<Map<String, Object>> updateTodo(
+      @PathVariable Long id,
+      @Valid @RequestBody TodoUpdateRequestDto dto,
+      BindingResult bindingResult) {
+    this.todoService.updateTodo(id, dto.getTitle(), dto.getContents());
+    Map<String, Object> response = new HashMap<>();
+
+    // validation 에러가 있는 경우, 메시지 출력
+    if (bindingResult.hasErrors()) {
+      String errorMessage = Parse.toString(bindingResult.getFieldErrors());
+      response.put("validationError", errorMessage);
+
+      return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    // 수정일이 반영된 데이터가 필요하기 때문에 다시 조회를 실행
+    TodoResponseDto todoResponseDto = this.todoService.findById(id);
+    response.put("todoData", todoResponseDto);
+
+    return new ResponseEntity<>(response, HttpStatus.OK);
+  }
+
+  /**
+   * 일정 삭제 API
+   *
+   * @param id 일정 ID
+   * @return 삭제 결과
+   */
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteTodo(@PathVariable Long id) {
+    this.todoService.deleteTodo(id);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+}
---
+++ src/main/java/org/example/develop_todo/lv6/service/UserService.java
@@ -0,0 +1,105 @@
+package org.example.develop_todo.lv6.service;
+
+import jakarta.transaction.Transactional;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.example.develop_todo.lv6.config.PasswordEncoder;
+import org.example.develop_todo.lv6.dto.user.UserResponseDto;
+import org.example.develop_todo.lv6.entity.User;
+import org.example.develop_todo.lv6.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+
+  /**
+   * 사용자 생성
+   *
+   * @param username 사용자 이름
+   * @param password 비밀번호
+   * @param email 이메일
+   * @return 저장된 사용자 정보
+   */
+  public UserResponseDto createUser(String username, String password, String email) {
+    // 비밀번호 암호화 적용
+    String encodedPassword = this.passwordEncoder.encode(password);
+
+    User user = new User(username, encodedPassword, email);
+    User savedUser = this.userRepository.save(user);
+
+    return UserResponseDto.toDto(savedUser);
+  }
+
+  /**
+   * 사용자 전체 조회
+   *
+   * @return 조회된 사용자 정보
+   */
+  public List<UserResponseDto> findAllUser() {
+    return this.userRepository.findAll().stream().map(UserResponseDto::toDto).toList();
+  }
+
+  /**
+   * 사용자 1명 조회
+   *
+   * @param id 사용자 ID
+   * @return 조회된 사용자 정보
+   */
+  public UserResponseDto findById(Long id) {
+    User findUser = this.userRepository.findByIdElseOrThrow(id);
+    return UserResponseDto.toDto(findUser);
+  }
+
+  /**
+   * 사용자 정보 수정
+   *
+   * @param id 사용자 ID
+   * @param username 사용자 이름
+   * @param password 비밀번호
+   * @param email 이메일
+   * @return 수정된 사용자 정보
+   */
+  @Transactional
+  public UserResponseDto updateUser(Long id, String username, String password, String email) {
+    User findUser = this.userRepository.findByIdElseOrThrow(id);
+    // 비밀번호 암호화 적용
+    String encodedPassword = this.passwordEncoder.encode(password);
+    findUser.updateUser(username, encodedPassword, email);
+
+    return UserResponseDto.toDto(findUser);
+  }
+
+  /**
+   * 사용자 삭제
+   *
+   * @param id 사용자 ID
+   */
+  public void deleteUser(Long id) {
+    User findUser = this.userRepository.findByIdElseOrThrow(id);
+    this.userRepository.delete(findUser);
+  }
+
+  /**
+   * 사용자 조회 (로그인용)
+   *
+   * @param password 비밀번호
+   * @param email 이메일
+   * @return 조회된 사용자 정보
+   */
+  public UserResponseDto validLoginUserInfo(String password, String email) {
+    User findUser = this.userRepository.findByEmailElseOrThrow(email);
+    boolean isMatchesPassword = this.passwordEncoder.matches(password, findUser.getPassword());
+
+    if (!isMatchesPassword) {
+      throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.");
+    }
+
+    return UserResponseDto.toDto(findUser);
+  }
+}
